970b4ccf954c8c7359ac55722412fd2ef3707e7e
hugo de araujo <hugo.saraujo@tutanota.com>
2023-12-06 12:31:54 -0300

    Tornando o menu mais interativa e fechando-o apenas quando o opção sair for selecionada

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner leitura = new Scanner(System.in);
+        int opcaoSelecionada = -1;
 
         System.out.println("Bem vindo ao aplicativo de controle finaceiro");
         System.out.print("Digite o seu nome: ");
@@ -18,34 +19,51 @@ public class Main {
                 0- Sair
                 """;
 
-        System.out.println(menuDeOpcoes);
+        while(opcaoSelecionada != 0){
 
-        System.out.print("Insira o número da operação que quer realizar: ");
-        int opcaoValida = leitura.nextInt();
-        switch (opcaoValida) {
-            case 1:
-                System.out.println("Opção 1 - Consultar dados");
-                String consultaDeSaldo = """
+            System.out.println(menuDeOpcoes);
+
+            System.out.print("Insira o número da operação que quer realizar: ");
+            opcaoSelecionada = leitura.nextInt();
+
+            switch (opcaoSelecionada) {
+                case 1:
+                    System.out.println("Opção 1 - Consultar dados");
+                    String consultaDeSaldo = """
                         Titular: %s
                         Saldo: %.2f
                         """.formatted(nome, saldo);
-                System.out.println(consultaDeSaldo);
-                break;
-            case 2:
-                System.out.println("Opção 2 - Depositar valor");
-                break;
-            case 3:
-                System.out.println("Opção 3 - Sacar valor");
-                break;
-            case 0:
-                System.out.println("Saindo da Aplicação...");
-                System.out.println("Tchau, tchau");
-                break;
-            default:
-                System.out.println("Essa não é uma opção válida");
-                System.out.println("Tente novamente");
-                break;
-        }
-    }
+                    System.out.println(consultaDeSaldo);
+                    break;
+                case 2:
+                    System.out.println("Opção 2 - Depositar valor");
+                    System.out.printf("%s, insira o valor que deseja depositar: ".formatted(nome));
+                    double deposito = leitura.nextDouble();
+                    saldo += deposito;
+                    System.out.println("Depósito realizado com sucesso!");
+                    break;
+                case 3:
+                    System.out.println("Opção 3 - Sacar valor");
+                    System.out.printf("%s, informe o valor do saque: ".formatted(nome));
+                    double saque = leitura.nextDouble();
+                    if (saque > saldo) {
+                        System.out.println("Saldo insuficiente, tente sacar um valor menor");
+                    } else {
+                        saldo -= saque;
+                        System.out.println("Saque realizado com sucesso!");
+                    }
 
+                    break;
+                case 0:
+                    System.out.println("Saindo da Aplicação...");
+                    System.out.println("Tchau, tchau");
+                    break;
+                default:
+                    System.out.println("Essa não é uma opção válida");
+                    System.out.println("Tente novamente");
+                    break;
+            }
+        }
+
+    }
 }
